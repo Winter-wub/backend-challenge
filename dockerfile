b2affd1a3b5d932202fa39efd87c2f804cc68699
StratosBlur <wave_onlyone@hotmail.com>
2019-02-14 14:30:37 +0700

    Add docker for deploy 🐳

---
+++ dockerfile
@@ -0,0 +1,2 @@
+FROM python:3.7-slim
+ENV GOOGLE_APPLICATION_CREDENTIALS=/usr/local/credentials.json
--- utils/firebase.py
+++ utils/firebase.py
@@ -1,10 +1,9 @@
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
+from firebase_admin import credentials, firestore
+import os
 # Use a service account
-cred = credentials.Certificate(
-    'C:/Users/wave_/developer-challange-1bf4eca39862.json')
+cred = credentials.Certificate(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
 firebase_admin.initialize_app(cred)
+
 
 db = firestore.client()

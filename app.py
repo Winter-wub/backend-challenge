ef593bc62ff06a25ef8709dffcd45b59626ee0ee
StratosBlur <wave_onlyone@hotmail.com>
2019-02-15 23:27:17 +0700

    Add cors to all route 🌍

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, url_for
+from flask import Flask
+from flask_cors import CORS
 from utils.firebase import db
 from routes.products import product
 from routes.orders import order
 from routes.customers import customers
 
 app = Flask(__name__)
-
+CORS(app)
 app.register_blueprint(product)
 app.register_blueprint(order)
 app.register_blueprint(customers)
+
 app.run(host='0.0.0.0', port=5000)

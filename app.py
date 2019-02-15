55dfba648728d254f72179301e4279451593b0a3
StratosBlur <wave_onlyone@hotmail.com>
2019-02-15 23:40:29 +0700

    Add update request send header cors 🐱‍🐉

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ from routes.customers import customers
 
 app = Flask(__name__)
 CORS(app)
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+
 app.register_blueprint(product)
 app.register_blueprint(order)
 app.register_blueprint(customers)
